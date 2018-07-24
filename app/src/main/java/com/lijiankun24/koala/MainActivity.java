ac8bebc3963b4b609f8b5498aeda819d51248c77
lijiankun03 <lijiankun03@meituan.com>
2018-07-24 19:31:48 +0800

    first commit

---
+++ app/src/main/java/com/lijiankun24/koala/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lijiankun24.koala;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
