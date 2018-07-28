906144d0778887c6a5ced1220902c89f1b306033
lijiankun24 <jiankunli24@gmail.com>
2018-07-28 23:04:07 +0800

    Mod：mod Gradle Plugin.

--- buildSrc/src/main/groovy/com/lijiankun24/koalaplugin/Koala.groovy
+++ buildSrc/src/main/groovy/com/lijiankun24/koalaplugin/Koala.groovy
@@ -1,6 +1,5 @@
 package com.lijiankun24.koalaplugin
 
-import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 /**
@@ -8,11 +7,11 @@ import org.gradle.api.Project
  * <p>
  * Created by lijiankun on 18/4/14.
  */
-public class Koala implements Plugin<Project> {
+class Koala implements Plugin<Project> {
     @Override
-    public void apply(Project project) {
+    void apply(Project project) {
         project.logger.error("====== Hello Gradle Plugin =======")
-        def android = project.extensions.findByType(AppExtension.class)
-        android.registerTransform(new PreClass(project))
+//        def android = project.extensions.findByType(AppExtension.class)
+//        android.registerTransform(new PreClass(project))
     }
 }
--- buildSrc/src/main/groovy/com/lijiankun24/koalaplugin/PreClass.groovy
+++ buildSrc/src/main/groovy/com/lijiankun24/koalaplugin/PreClass.groovy
@@ -1,5 +1,7 @@
 package com.lijiankun24.koalaplugin
 
+import com.android.build.api.transform.DirectoryInput
+import com.android.build.api.transform.Format
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
@@ -7,6 +9,7 @@ import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformInvocation
 import com.android.build.gradle.internal.pipeline.TransformManager
 import org.gradle.api.Project
+import org.gradle.internal.impldep.org.apache.ivy.util.FileUtil
 
 class PreClass extends Transform {
 
@@ -40,7 +43,13 @@ class PreClass extends Transform {
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
 //        super.transform(transformInvocation)
         transformInvocation.inputs.each { TransformInput input ->
+            input.directoryInputs.each { DirectoryInput directoryInput ->
 
+                def dest = transformInvocation.outputProvider.getContentLocation(directoryInput.name,
+                    directoryInput.contentTypes, directoryInput.scopes, Format.DIRECTORY)
+
+                FileUtil.copy(directoryInput.file, dest)
+            }
         }
     }
 }
