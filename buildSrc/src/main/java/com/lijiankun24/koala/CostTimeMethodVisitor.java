578abf55c243a7a0f31b15c6cd36e3a1f49278b8
lijiankun03 <lijiankun03@meituan.com>
2018-08-27 20:41:33 +0800

    Add：初步添加统计方法执行时间代码.

--- buildSrc/src/main/java/com/lijiankun24/koala/CostTimeMethodVisitor.java
+++ buildSrc/src/main/java/com/lijiankun24/koala/CostTimeMethodVisitor.java
@@ -1,9 +1,10 @@
 package com.lijiankun24.koala;
 
+import com.lijiankun24.koala.core.MethodCache;
+
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.AdviceAdapter;
 
@@ -28,12 +29,15 @@ class CostTimeMethodVisitor extends AdviceAdapter {
 
     private String[] methodParametersNames;
 
+    private int methodId;
+
+
     CostTimeMethodVisitor(int api, MethodVisitor mv, int access, String name, String desc) {
         super(api, mv, access, name, desc);
         this.methodName = name;
         this.access = access;
+        methodId = MethodCache.Request();
         methodParameterCount = Type.getArgumentTypes(desc).length;
-
         methodParametersNames = new String[methodParameterCount];
         System.out.println("CostTimeMethodVisitor name ================ " + name);
     }
@@ -77,35 +81,6 @@ class CostTimeMethodVisitor extends AdviceAdapter {
     @Override
     protected void onMethodExit(int opcode) {
         if (isInjected) {
-            String msg;
-            switch (opcode) {
-                case Opcodes.RETURN:
-                    msg = "RETURN";
-                    break;
-                case Opcodes.IRETURN:
-                    msg = "IRETURN";
-                    break;
-                case Opcodes.FRETURN:
-                    msg = "FRETURN";
-                    break;
-                case Opcodes.DRETURN:
-                    msg = "DRETURN";
-                    break;
-                case Opcodes.LRETURN:
-                    msg = "LRETURN";
-                    break;
-                case Opcodes.ATHROW:
-                    msg = "ATHROW";
-                    break;
-                case Opcodes.ARETURN:
-                    msg = "ARETURN";
-                    break;
-                default:
-                    msg = "default";
-                    break;
-            }
-            System.out.println("the method's onMethodExit opcode is " + msg);
-
             if (opcode == RETURN) {
                 visitInsn(ACONST_NULL);
             } else if (opcode == ARETURN || opcode == ATHROW) {
@@ -118,29 +93,32 @@ class CostTimeMethodVisitor extends AdviceAdapter {
                 }
                 box(Type.getReturnType(this.methodDesc));
             }
-            // java.lang.NoClassDefFoundError
-            visitIntInsn(SIPUSH, opcode);
-            visitMethodInsn(INVOKESTATIC, "com/lijiankun24/koala/Printer",
-                    "onExit", "(Ljava/lang/Object;I)V", false);
-
-            for (int i = 0; i < methodParametersNames.length; i++) {
-                System.out.println("the method's " + i + " name is " + methodParametersNames[i]);
-            }
-            int durationId = newLocal(Type.LONG_TYPE);
-            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
             mv.visitVarInsn(LLOAD, startTimeId);
-            mv.visitInsn(LSUB);
-            mv.visitVarInsn(LSTORE, durationId);
-            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
-            mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
-            mv.visitInsn(DUP);
-            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
-            mv.visitLdcInsn("The cost time of " + methodName + " is ");
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
-            mv.visitVarInsn(LLOAD, durationId);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
-            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+            mv.visitLdcInsn(methodId);
+            visitMethodInsn(INVOKESTATIC, "com/lijiankun24/koala/core/MethodCache",
+                    "updateMethodDurationAndResult", "(Ljava/lang/Object;JI)V", false);
+            mv.visitLdcInsn(methodId);
+            visitMethodInsn(INVOKESTATIC, "com/lijiankun24/koala/core/MethodCache",
+                    "printMethodInfo", "(I)V", false);
+
+//            for (int i = 0; i < methodParametersNames.length; i++) {
+//                System.out.println("the method's " + i + " name is " + methodParametersNames[i]);
+//            }
+//            int durationId = newLocal(Type.LONG_TYPE);
+//            mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J", false);
+//            mv.visitVarInsn(LLOAD, startTimeId);
+//            mv.visitInsn(LSUB);
+//            mv.visitVarInsn(LSTORE, durationId);
+//            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+//            mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
+//            mv.visitInsn(DUP);
+//            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
+//            mv.visitLdcInsn("The cost time of " + methodName + " is ");
+//            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(Ljava/lang/String;)Ljava/lang/StringBuilder;", false);
+//            mv.visitVarInsn(LLOAD, durationId);
+//            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(J)Ljava/lang/StringBuilder;", false);
+//            mv.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
+//            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
         }
     }
 }
